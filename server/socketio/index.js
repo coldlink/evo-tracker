b2af1a0d51878962467733f7c49887412da5919c
Mahesh Makani <mahesh.makani@netnatives.co.uk>
2017-01-27 11:10:02 +0000

    connect count

--- server/socketio/index.js
+++ server/socketio/index.js
@@ -3,9 +3,16 @@
 module.exports = function (server) {
   const io = require('socket.io')(server)
   const API = require('../api')
+  let users = 0
 
   io.on('connection', (socket) => {
+    users++
+    console.log('User Connect: ', users)
     socket.emit('data', API.getData())
+    socket.on('disconnect', () => {
+      users--
+      console.log('User Disconnect: ', users)
+    })
   })
 
   setInterval(() => {
